7f9fc9da35541fa935ba04ebaed969d86cce41dd
Svyatoslav Ovsyannikov <optatt@yandex.ru>
2015-12-18 11:07:31 +0300

    Added vars and mixins

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import HomeApp from "./blocks/home/App";
 
-var el = document.getElementsByClassName("home__wrapper");
+var el = document.getElementsByClassName("page");
 
 if (el.length == 1)
     React.render(<HomeApp />, el[0]);
